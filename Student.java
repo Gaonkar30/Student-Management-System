983e832e47a7134f879540aedfbb9663d7353579
AkashGGaonkar <akashggaonkar@gmail.com>
2024-11-05 23:39:34 +0530

    Student class

---
+++ Student.java
@@ -0,0 +1,12 @@
+package s.m.s;
+
+public class Student {
+    private int id;
+    private String name;
+    private int grade;
+    private int feesPaid;
+    private int totalFees;
+    // constructor
+    
+
+}
